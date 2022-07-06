f9986a4214bd72ffc9d101f5232b766550f5a828
Admin <admin.push@hackerrank.workspace>
2022-07-06 09:23:59 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/index.js
+++ src/index.js
@@ -8,5 +8,3 @@ import "./js/script"
 if (module.hot) {
     module.hot.accept();
 }
-
-console.log('DOM Loaded');
---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+const list = [];
+
+const addToList = () => {
+    const text = getElem('input').value;
+    if(text.trim() === ""){
+        return getElem('alert').style.display  = 'block';
+    }
+    list.push(text);
+    addedToList();
+    reRenderList();
+}
+const reRenderList = () => {
+    getElem('list').innerHTML = '';
+    const elements = [];
+
+    list.forEach((item)=> {
+        elements.push(`<li>${item}</li>`);
+    });
+    getElem('list').innerHTML = elements.join('');
+}
+const getElem = (id) => {
+    return document.getElementById(id);
+}
+const addedToList = () => {
+    getElem('input').value = "";
+    getElem('alert').style.display = 'none';
+}
